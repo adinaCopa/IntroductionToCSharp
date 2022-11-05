7dca75c39ef0cf4245275d5cf1ea5354c84d2536
Adina Copacianu <Adina.Copacianu@centric.eu>
2022-11-05 23:59:34 +0200

    Added yagni

---
+++ Design_Principles/YouAintGonnaNeedItPrinciple/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace YouAintGonnaNeedItPrinciple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+
+            var violation = new YAGNI_violation();
+            var sum = violation.CalculateSum(a, b);
+
+            // you don't need a special method to calculate a sum
+
+            var simpleSum = a + b;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Design_Principles/YouAintGonnaNeedItPrinciple/YAGNI_violation.cs
@@ -0,0 +1,10 @@
+﻿namespace YouAintGonnaNeedItPrinciple
+{
+    internal class YAGNI_violation
+    {
+        public int CalculateSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
