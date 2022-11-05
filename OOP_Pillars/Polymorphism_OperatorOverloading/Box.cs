97c31c3fccf606c0c9cf5c20e6f43edbfa0a4014
Adina Copacianu <Adina.Copacianu@centric.eu>
2022-11-05 19:27:55 +0200

    Add polymorphism - operator

---
+++ OOP_Pillars/Polymorphism_OperatorOverloading/Box.cs
@@ -0,0 +1,27 @@
+﻿namespace Polymorphism_OperatorOverloading
+{
+    internal class Box
+    {
+        private int height;
+        private int width;
+
+        public Box(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public static Box operator + (Box box1, Box box2)
+        {
+            var newHeight = box1.height + box2.height;
+            var newWidth = box1.width + box2.width;
+
+            return new Box(newHeight, newWidth);
+        }
+
+        public string DisplayInfo()
+        {
+            return $"Box with h = {this.height}, w = {this.width}";
+        }
+    }
+}
---
+++ OOP_Pillars/Polymorphism_OperatorOverloading/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Polymorphism_OperatorOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var box1 = new Box(5, 7);
+            var box2 = new Box(9, 2);
+
+            Console.WriteLine("Box 1: " + box1.DisplayInfo());
+            Console.WriteLine("Box 2: " + box2.DisplayInfo());
+
+            var box3 = box1 + box2;
+            Console.WriteLine("Box 3: " + box3.DisplayInfo());
+
+            Console.ReadLine();
+        }
+    }
+}
