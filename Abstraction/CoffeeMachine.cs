16c3f1fb15eaea17024bd211652e566f773938da
adinaCopa <79633449+adinaCopa@users.noreply.github.com>
2022-11-05 17:39:31 +0200

    Add files via upload

---
+++ Abstraction/CoffeeMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Abstraction
+{
+    internal class CoffeeMachine
+    {
+        public void MakeCoffee()
+        {
+            this.AddCoffee();
+            this.AddWater();
+            this.BoilCoffee();
+        }
+
+        private void AddWater()
+        {
+            Console.WriteLine("Water added.");
+        }
+
+        private void AddCoffee()
+        {
+            Console.WriteLine("Coffee added.");
+        }
+
+        private void BoilCoffee()
+        {
+            Console.WriteLine("Coffee done.");
+        }
+    }
+}
---
+++ Encapsulation_Violation/BankAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace Encapsulation_Violation
+{
+    internal class BankAccount
+    {
+        public double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+    }
+}
